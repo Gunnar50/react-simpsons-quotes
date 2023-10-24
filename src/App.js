6ba4df709471736c82b90d537d5c72f881d19884
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-10-24 17:55:45 +0100

    api access

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import axios from "axios";
 import React, { Component } from "react";
+import Card from "./components/Card";
 
 const API_URL = "https://thesimpsonsquoteapi.glitch.me/quotes";
 
-class App extends Component {
+export default class App extends Component {
 	state = {};
 
 	async componentDidMount() {
 		try {
 			const { data } = await axios.get(`${API_URL}?count=10`);
-			this.setState({ simpsons: data });
+			this.setState({ data });
 		} catch (e) {
 			console.log("Looks like the API is down!");
 		}
 	}
 
 	render() {
-		console.log(this.state);
-		return <></>;
+		// console.log(this.state);
+		return <Card data={this.state.data} />;
 	}
 }
