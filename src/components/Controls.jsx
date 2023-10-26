a76d3f0fadf2b7e6d52236ece05658ec29852d0f
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-10-26 08:46:49 +0100

    refractor

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
-import "../App.css";
+import "../styles/App.scss";
 import Button from "./Button";
 
 export default class Controls extends Component {
 	render() {
+		const { id, liked, onLiked, onDelete } = this.props;
 		return (
 			<div className="controls">
-				<Button text="Like" type="like" />
-				<Button text="Delete" type="delete" />
+				<Button text="Like" id={id} liked={liked} onCliked={onLiked} />
+				<Button text="Delete" onCliked={onDelete} />
 			</div>
 		);
 	}
