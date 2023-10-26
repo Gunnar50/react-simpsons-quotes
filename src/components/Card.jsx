1d99977dfd2deace68552fb99a623432bf235665
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-10-26 08:46:45 +0100

    refractor

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,48 @@
 import React, { Component } from "react";
-import "../App.css";
-import Character from "./Character";
+import "../styles/App.scss";
+import Controls from "./Controls";
+import Image from "./Image";
+import Name from "./Name";
+import Quote from "./Quote";
 
 export default class Card extends Component {
 	render() {
-		console.log(this.props.data);
-		return this.props.data.map((character) => (
-			<div className="card">
-				<Character {...character} />
+		const {
+			character: name,
+			image,
+			characterDirection,
+			quote,
+			id,
+			liked,
+			onLiked,
+			onDelete,
+		} = this.props;
+		return (
+			<div className={`card ${characterDirection.toLowerCase()}`}>
+				{console.log(characterDirection)}
+				{characterDirection === "Left" && (
+					<div className="image">
+						<Image image={image} alt={name} />
+					</div>
+				)}
+
+				<div className="quote">
+					<Quote quote={quote} />
+					<Name name={name} />
+					<Controls
+						id={id}
+						liked={liked}
+						onLiked={onLiked}
+						onDelete={onDelete}
+					/>
+				</div>
+
+				{characterDirection === "Right" && (
+					<div className="image">
+						<Image image={image} alt={name} />
+					</div>
+				)}
 			</div>
-		));
+		);
 	}
 }
