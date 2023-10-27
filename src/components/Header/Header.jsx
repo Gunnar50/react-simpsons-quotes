7ec08023d7d8a81d6b18adafa2d9068cd1ded85f
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-10-27 13:44:36 +0100

    added header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import logo from "../../assets/images/The-Simpsons-Logo-PNG.png";
+import Filter from "../Filter";
+import Sort from "../Sort";
+
+function Header(props) {
+	return (
+		<header>
+			<div className="container">
+				<img src={logo} />
+
+				{/* sort component */}
+				<Sort onSortSelectionChange={props.onSortSelectionChange} />
+
+				{/* filter component */}
+				<Filter onFilterChange={props.onFilterChange} />
+
+				<h2>Total liked count: {props.likedCount}</h2>
+			</div>
+		</header>
+	);
+}
+
+export default Header;
