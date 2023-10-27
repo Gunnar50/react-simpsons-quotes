b6b541b55d2a6fe66c7d48ea903d4ff2bc59bf07
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-10-27 13:44:45 +0100

    moved to header

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -3,8 +3,12 @@ import React from "react";
 const Filter = (props) => {
 	return (
 		<div className="filter">
-			<label htmlFor="filter">Search by name</label>
-			<input type="text" id="filter" onInput={props.onFilterChange} />
+			<input
+				placeholder="Search by name"
+				type="text"
+				id="filter"
+				onInput={props.onFilterChange}
+			/>
 		</div>
 	);
 };
