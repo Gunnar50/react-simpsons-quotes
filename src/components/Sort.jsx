425b94d27968b32758278579eaaeda2703f542f7
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-10-27 13:44:49 +0100

    moved to header

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 const Sort = (props) => {
 	return (
-		<div className="filter">
-			<label htmlFor="sort">Sort by</label>
+		<div className="sort">
 			<select name="sort" id="sort" onChange={props.onSortSelectionChange}>
+				<option value="sort-by" disabled>
+					-- Sort by --
+				</option>
 				<option value="name-az">Name - A to Z</option>
 				<option value="name-za">Name - Z to A</option>
 				<option value="quote-az">Quote - A to Z</option>
