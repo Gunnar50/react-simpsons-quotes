0b49554bdb6fd26e1ed4383b64b54b53a0caac5b
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-10-27 09:47:46 +0100

    added filter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import axios from "axios";
 import React, { Component } from "react";
 import Card from "./components/Card";
+import Filter from "./components/Filter";
+import Sort from "./components/Sort";
 import "./styles/App.scss";
+import { filterByName } from "./utils/filter";
 import { addIDs } from "./utils/generateUniqueID";
 import { sortData } from "./utils/sort";
 
 const API_URL = "https://thesimpsonsquoteapi.glitch.me/quotes";
 
 export default class App extends Component {
-	state = { likedCount: 0, sort: "az" };
+	state = { likedCount: 0, filter: "" };
 
 	updateCount = (newData) => {
 		const newLikedCount = newData.reduce((count, character) => {
@@ -33,6 +36,10 @@ export default class App extends Component {
 		this.setState({ sort: e.target.value });
 	};
 
+	onFilterChange = (e) => {
+		this.setState({ filter: e.target.value });
+	};
+
 	async componentDidMount() {
 		try {
 			const { data } = await axios.get(`${API_URL}?count=10`);
@@ -44,25 +51,25 @@ export default class App extends Component {
 	}
 
 	render() {
-		const { data, likedCount, sort } = this.state;
+		const { data, likedCount, sort, filter } = this.state;
 		if (!data) return "Loading...";
 
 		const _data = [...data];
+		const filtered = filterByName(_data, filter);
 
-		sortData(_data, sort);
+		sortData(filtered, sort);
 
 		return (
 			<div className="container">
-				<div className="filter">
-					<label htmlFor="sort">Sort by</label>
-					<select name="sort" id="sort" onChange={this.onSortSelectionChange}>
-						<option value="az">A to Z</option>
-						<option value="za">Z to A</option>
-					</select>
-				</div>
+				{/* sort component */}
+				<Sort onSortSelectionChange={this.onSortSelectionChange} />
+
+				{/* filter component */}
+				<Filter onFilterChange={this.onFilterChange} />
+
 				<h2>Total liked count: {likedCount}</h2>
 				<div className="cards-container">
-					{_data.map((character, i) => {
+					{filtered.map((character, i) => {
 						return (
 							<Card
 								key={i}
