38618cb69c1cabf2108327278ce3f80e0d82ec9d
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-10-28 15:17:19 +0100

    changed to functional component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,39 +1,34 @@
-import React, { Component } from "react";
+import React from "react";
 import Controls from "./Controls";
 import Image from "./Image";
 import Name from "./Name";
 import Quote from "./Quote";
 
-export default class Card extends Component {
-	render() {
-		const {
-			character: name,
-			image,
-			characterDirection,
-			quote,
-			id,
-			liked,
-			onLiked,
-			onDelete,
-		} = this.props;
+const Card = (props) => {
+	const {
+		character: name,
+		image,
+		characterDirection,
+		quote,
+		id,
+		liked,
+		onLiked,
+		onDelete,
+	} = props;
 
-		return (
-			<div className={`card ${characterDirection.toLowerCase()}`}>
-				<div className="image">
-					<Image image={image} alt={name} />
-				</div>
-
-				<div className="quote">
-					<Quote quote={quote} />
-					<Name name={name} />
-					<Controls
-						id={id}
-						liked={liked}
-						onLiked={onLiked}
-						onDelete={onDelete}
-					/>
-				</div>
+	return (
+		<div className={`card ${characterDirection.toLowerCase()}`}>
+			<div className="image">
+				<Image image={image} alt={name} />
 			</div>
-		);
-	}
-}
+
+			<div className="quote">
+				<Quote quote={quote} />
+				<Name name={name} />
+				<Controls id={id} liked={liked} onLiked={onLiked} onDelete={onDelete} />
+			</div>
+		</div>
+	);
+};
+
+export default Card;
