e61a29d6113825542456a683e3caf5af78632353
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-10-28 15:17:25 +0100

    changed to functional component

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,14 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 import Button from "./Button";
 
-export default class Controls extends Component {
-	render() {
-		const { id, liked, onLiked, onDelete } = this.props;
-		return (
-			<div className="controls">
-				<Button text="Like" id={id} liked={liked} onCliked={onLiked} />
-				<Button text="Delete" onCliked={onDelete} />
-			</div>
-		);
-	}
-}
+const Controls = (props) => {
+	const { id, liked, onLiked, onDelete } = props;
+	return (
+		<div className="controls">
+			<Button text="Like" id={id} liked={liked} onCliked={onLiked} />
+			<Button text="Delete" onCliked={onDelete} />
+		</div>
+	);
+};
+
+export default Controls;
