e2247eff6e6d30153665a73061934f9449e85d6c
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-10-28 15:10:47 +0100

    refractor

--- src/components/Header/Filter.jsx
+++ src/components/Header/Filter.jsx
@@ -2,24 +2,12 @@ import React from "react";
 
 const Filter = (props) => {
 	return (
-		<div className="filter">
-			<input
-				placeholder="Search by name"
-				type="text"
-				id="filter"
-				onInput={props.onFilterChange}
-			/>
-
-			<select
-				name="filter-type"
-				id="filter-type"
-				onChange={props.onFilterSelectionChange}
-			>
-				<option value="filter-by">-- Filter by --</option>
-				<option value="name-filter">Name</option>
-				<option value="quote-filter">Quote</option>
-			</select>
-		</div>
+		<input
+			placeholder={`Search by ${props.filterType ? props.filterType : "..."}`}
+			type="text"
+			id="filter"
+			onInput={props.onFilterChange}
+		/>
 	);
 };
 
