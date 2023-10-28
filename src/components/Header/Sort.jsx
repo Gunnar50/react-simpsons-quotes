f540bfbb1046becdbccee685302a5e1b80653cc3
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-10-28 15:11:01 +0100

    refractor

--- src/components/Header/Sort.jsx
+++ src/components/Header/Sort.jsx
@@ -2,15 +2,13 @@ import React from "react";
 
 const Sort = (props) => {
 	return (
-		<div className="sort">
-			<select name="sort" id="sort" onChange={props.onSortSelectionChange}>
-				<option value="sort-by">-- Sort by --</option>
-				<option value="name-az">Name - A to Z</option>
-				<option value="name-za">Name - Z to A</option>
-				<option value="quote-az">Quote - A to Z</option>
-				<option value="quote-za">Quote - Z to A</option>
-			</select>
-		</div>
+		<select name="sort" id="sort" onChange={props.onSortSelectionChange}>
+			<option value="sort-by">-- Sort by --</option>
+			<option value="name-az">Name - A to Z</option>
+			<option value="name-za">Name - Z to A</option>
+			<option value="quote-az">Quote - A to Z</option>
+			<option value="quote-za">Quote - Z to A</option>
+		</select>
 	);
 };
 
